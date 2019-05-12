2587f17e277a890922cf812e1b10b5cf8c0f1dc7
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2019-05-12 10:24:52 -0300

    Aulas do curso parte 2 de c#

---
+++ 03-ByteBank/ContaCorrente.cs
@@ -0,0 +1,8 @@
+﻿public class ContaCorrente
+{
+    public string titular;
+    public int numeroAgencia;
+    public int numero;
+    public double saldo = 100;
+
+}
---
+++ 03-ByteBank/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaRicardo = new ContaCorrente();
+            contaRicardo.titular = "Ricardo";
+            contaRicardo.numeroAgencia = 8898;
+            contaRicardo.numero = 454212121;
+
+            ContaCorrente contaRicardoMoreira = new ContaCorrente();
+            contaRicardoMoreira.titular = "Ricardo";
+            contaRicardoMoreira.numeroAgencia = 8898;
+            contaRicardoMoreira.numero = 454212121;
+
+            Console.WriteLine("Igualdade de tipo de referência: " + (contaRicardo == contaRicardoMoreira));
+
+            int idade = 31;
+            int idadeNovamente = 31;
+
+            Console.WriteLine(idade == idadeNovamente);
+
+            contaRicardo = contaRicardoMoreira;
+
+            Console.WriteLine("Igualdade de tipo de valor: " + (contaRicardo == contaRicardoMoreira));
+
+            contaRicardoMoreira.saldo = 300;
+            Console.WriteLine(contaRicardo.saldo);
+            Console.WriteLine(contaRicardoMoreira.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
