8e7d68dc2794a78c43dfc210f9e9c692b46f18ab
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2019-05-18 09:53:02 -0300

    Namespaces e composição de classes

--- 05-ByteBank/Program.cs
+++ 05-ByteBank/Program.cs
@@ -11,6 +11,25 @@ namespace _05_ByteBank
         static void Main(string[] args)
         {
             Cliente ricardo = new Cliente();
+
+            ricardo.nome = "Ricardo";
+            ricardo.cpf = "123.456.789.01";
+            ricardo.profissao = "Analista de Sistemas";
+
+            ContaCorrente contaRicardo = new ContaCorrente();
+
+            contaRicardo.titular = ricardo;
+
+            contaRicardo.saldo = 500;
+            contaRicardo.numeroAgencia = 4545;
+            contaRicardo.numero = 2313221;
+
+            contaRicardo.titular.nome = "Ricardo Moreira Soares";
+
+            Console.WriteLine(ricardo.nome);
+            Console.WriteLine(contaRicardo.titular.nome);
+
+            Console.ReadKey();
         }
     }
 }
