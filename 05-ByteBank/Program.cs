699fc6d6e2d98b8269bd368e0528657d72a1ce6e
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2019-05-18 10:03:03 -0300

    Referencias nulas

--- 05-ByteBank/Program.cs
+++ 05-ByteBank/Program.cs
@@ -10,24 +10,38 @@ namespace _05_ByteBank
     {
         static void Main(string[] args)
         {
-            Cliente ricardo = new Cliente();
+            //Cliente ricardo = new Cliente();
 
-            ricardo.nome = "Ricardo";
-            ricardo.cpf = "123.456.789.01";
-            ricardo.profissao = "Analista de Sistemas";
+            //ricardo.nome = "Ricardo";
+            //ricardo.cpf = "123.456.789.01";
+            //ricardo.profissao = "Analista de Sistemas";
 
             ContaCorrente contaRicardo = new ContaCorrente();
 
-            contaRicardo.titular = ricardo;
+            //contaRicardo.titular = ricardo;
+
+            //contaRicardo.titular = new Cliente();
+
+            //contaRicardo.titular.nome = "Ricardo";
+            //contaRicardo.titular.cpf = "123.456.789.01";
+            //contaRicardo.titular.profissao = "Analista de Sistemas";
 
             contaRicardo.saldo = 500;
             contaRicardo.numeroAgencia = 4545;
             contaRicardo.numero = 2313221;
 
-            contaRicardo.titular.nome = "Ricardo Moreira Soares";
+            //contaRicardo.titular.nome = "Ricardo Moreira Soares";
 
-            Console.WriteLine(ricardo.nome);
-            Console.WriteLine(contaRicardo.titular.nome);
+            //Console.WriteLine(ricardo.nome);
+            //Console.WriteLine(contaRicardo.titular.nome);
+            //Console.WriteLine(contaRicardo.titular.cpf);
+
+            if (contaRicardo.titular == null)
+            {
+                Console.WriteLine("Ops, esta referencia é nula.");
+            }
+
+            Console.WriteLine(contaRicardo.titular);
 
             Console.ReadKey();
         }
