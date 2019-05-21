5e4d5a1da27bc790018b42ec6eceb43c4bc6a835
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2019-05-21 17:33:09 -0300

    Contrutores

---
+++ 07-ByteBank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(879, 8898977);
+
+            //conta.Agencia = 879;
+            //conta.Numero = 8898977;
+
+            //conta.Agencia = -10;
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadKey();
+        }
+    }
+}
