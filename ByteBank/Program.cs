f3a28336ad83aba912f0f09f4094fade7cea82d9
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2019-05-23 17:54:55 -0300

    Sobrecarga de método

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -11,14 +11,34 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            GerenciadorBonificacao gerador = new GerenciadorBonificacao();
+
             Funcionario funcionario = new Funcionario();
 
             funcionario.Nome = "Ricardo Moreira Soares";
             funcionario.CPF = "123.456.789.78";
             funcionario.Salario = 1000;
 
+            gerador.RegistrarFuncionario(funcionario);
+
+            Diretor diretor = new Diretor();
+
+            diretor.Nome = "Edivania Tatiele";
+            diretor.CPF = "123.574.789.78";
+            diretor.Salario = 5000;
+
+            gerador.RegistrarFuncionario(diretor);
+
             Console.WriteLine(funcionario.Nome);
             Console.WriteLine(funcionario.GetBonificacao());
+
+            Console.WriteLine(diretor.Nome);
+            Console.WriteLine(diretor.GetBonificacao());
+
+
+            Console.WriteLine(gerador.GetTotalBonificacao());
+            
+
             Console.ReadKey();
         }
     }
